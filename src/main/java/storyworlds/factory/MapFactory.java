dabefd47526b50815169939a098990494673bd5b
Nate Vaughan <nvaughanguitar@yahoo.com>
2016-09-12 23:31:19 -0500

    working on unlockable link

--- src/main/java/storyworlds/factory/MapFactory.java
+++ src/main/java/storyworlds/factory/MapFactory.java
@@ -18,8 +18,10 @@ public class MapFactory implements GameTextConstants {
         Location location = new ImmutableLocation(DEFAULT_LOCATION_TEXT,  createItems());
         location.setLink(Direction.UP, new BidirectionalLink(DEFAULT_LOCATION_TEXT_UP, location, getBlankLocation(Direction.DOWN, location)));
         UnlockableLink lockedLink = new UnlockableLink(DEFAULT_LOCATION_TEXT_DOWN, location, getBlankLocation(Direction.UP, location));
+        
         lockedLink.setRequiredItem(location, new UsableItem(ITEM_NAME, ITEM_PROPERTIES));
         location.setLink(Direction.DOWN, lockedLink);
+
         location.setLink(Direction.NORTH, new BidirectionalLink(DEFAULT_LOCATION_TEXT_NORTH, location, getBlankLocation(Direction.SOUTH, location)));
         location.setLink(Direction.SOUTH, new BidirectionalLink(DEFAULT_LOCATION_TEXT_SOUTH, location, getBlankLocation(Direction.NORTH, location)));
         location.setLink(Direction.EAST, new ImpassableLink(DEFAULT_LOCATION_TEXT_EAST));
